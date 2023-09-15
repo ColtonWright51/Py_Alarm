05ec4fff106c1b08e559de0b6ec9c38c5cd28e47
ColtonWright51 <cw717317@ohio.edu>
2023-09-14 23:44:19 -0400

    viva on

--- py_arlarm1.py
+++ py_arlarm1.py
@@ -12,7 +12,7 @@ def main():
     interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = interface.QueryInterface(IAudioEndpointVolume)
     volume.SetMasterVolumeLevel(-30.0, None)
-    # playsound(r'C:\Users\colto\Documents\GitHub\Py_Alarm\songs\Coldplay - Viva La Vida.mp3')
+    playsound(r'C:\Users\colto\Documents\GitHub\Py_Alarm\songs\Coldplay - Viva La Vida.mp3')
     # playsound(r'C:\Users\colto\Documents\GitHub\Py_Alarm\songs\loud_alarm.mp3')
     playsound(r'C:\Users\colto\Documents\GitHub\Py_Alarm\songs\loud_alarm.mp3')
     volume.SetMasterVolumeLevel(-18.0, None)
